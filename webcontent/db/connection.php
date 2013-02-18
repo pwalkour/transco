07769d00c8f432809f9f8a516acfefe232565303
Cotton Luc <imac@pilpoils.be>
2013-02-18 23:06:32 +0100

    not all info

--- webcontent/db/connection.php
+++ webcontent/db/connection.php
@@ -2,9 +2,5 @@
 # FileName="Connection_php_mysql.htm" 
 # Type="MYSQL" 
 # HTTP="true" 
-$hostname_ruine = ""; 
-$database_ruine = ""; 
-$username_ruine = ""; 
-$password_ruine = ""; 
 $con = mysql_pconnect($hostname_ruine, $username_ruine, $password_ruine) or die(mysql_error()); 
 ?>
