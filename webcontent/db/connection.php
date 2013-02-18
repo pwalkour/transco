817fd8b128826e57cc5a95cc6731784a526ea770
Cotton Luc <imac@pilpoils.be>
2013-02-18 23:07:23 +0100

    done

--- webcontent/db/connection.php
+++ webcontent/db/connection.php
@@ -2,5 +2,9 @@
 # FileName="Connection_php_mysql.htm" 
 # Type="MYSQL" 
 # HTTP="true" 
+$hostname_ruine = "localhost:8889"; 
+$database_ruine = "transco"; 
+$username_ruine = "root"; 
+$password_ruine = "pwalkour12"; 
 $con = mysql_pconnect($hostname_ruine, $username_ruine, $password_ruine) or die(mysql_error()); 
 ?>
